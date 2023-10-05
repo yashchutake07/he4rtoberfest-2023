aa655e2876e5517e077c20127e495deb612c58da
yashchutake07 <78459580+yashchutake07@users.noreply.github.com>
2023-10-05 21:04:00 +0530

    Create StringsandStringBuilder.java

---
+++ StringsandStringBuilder.java
@@ -0,0 +1,12 @@
+package StringsandStringBuilder;
+public class PreetyPrinting {
+    public static void main(String[] args) {
+        float a=431.3443f;
+        System.out.printf("%.2f",a);
+
+        System.out.printf("Hello I am %s and I have %s","yash","power");
+
+        System.out.printf("%.2f",Math.PI);
+        
+    }
+}
